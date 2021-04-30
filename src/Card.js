48874cebc8036d8ebc8c15962b9d8dbd91f746b0
sumitdeshmukh9922 <sumitdeshmukh9922@gmail.com>
2021-05-01 01:23:43 +0530

    2nd commit

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Card = ({title="", cardText="", buttonText=""}) => {
     return(
-        <div className="card" style={{width: "20rem"}}>
+        <div className="card" style={{width: "18rem"}}>
               <img
                 src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                 class="card-img-top"
